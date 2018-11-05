560bdf466bda92d08653d0b80fd71fab89e5afb1
LeonelAV <meirinho_leonel@hotmail.com>
2018-11-05 22:07:08 +0100

    git add --all

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
     return (
       <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
   }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
