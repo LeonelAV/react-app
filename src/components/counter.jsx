0fbb043f4de5e7c41530b57e0f7db3e3f124f675
LeonelAV <meirinho_leonel@hotmail.com>
2018-11-06 23:23:39 +0100

    onClick event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: []
+    tags: ["HERRERA", "MAREGA", "SOARES"]
   };
 
   styles = {
@@ -11,25 +11,34 @@ class Counter extends Component {
     fontWeight: "bold"
   };
 
-  rendertags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment clicked", this.state.count);
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.rendertags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "ZERO" : count;
+    // return this.state.count === 0 ? "ZERO" : this.state.count;
   }
 }
 
