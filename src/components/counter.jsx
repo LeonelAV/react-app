5aa3a71276a3350096c6dfdc226cd5c4c9042254
LeonelAV <meirinho_leonel@hotmail.com>
2018-11-19 21:00:04 +0100

    Delete button => onDelete event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   styles = {
     fontSize: 50,
     fontWeight: "bold"
   };
-
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
@@ -20,7 +19,6 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
         {this.props.children}
@@ -30,6 +28,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
